9556fa08ccda211d3b2275b065bf2ee604fc1c5f
James Clarke <james@jamesdavidclarke.com>
2025-10-29 13:00:00 +0000

    Added DaisyUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
-
+import daisyui from "daisyui";
 export default defineConfig({
-  plugins: [fresh(), tailwindcss()],
+  plugins: [fresh(), tailwindcss(), daisyui()],
   root: "./src",
 });
